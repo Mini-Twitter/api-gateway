0cdea3b395f355a2f3797e72cf7966d8f6225140
Jons <ozodjon160605@gmail.com>
2024-09-05 15:23:29 +0500

    Pull

--- api/router.go
+++ api/router.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"apigateway/api/handler"
+	"apigateway/api/middleware"
 	"apigateway/service"
 	"github.com/casbin/casbin/v2"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -29,7 +30,7 @@ func NewRouter(cfg *config.Config, casbin *casbin.Enforcer, conn *amqp.Channel, 
 
 	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
-	//router.Use(middleware.PermissionMiddleware(casbin))
+	router.Use(middleware.PermissionMiddleware(casbin))
 
 	a, err := service.NewService(cfg)
 	if err != nil {
